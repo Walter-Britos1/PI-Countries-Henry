db6b6f3f64eb727acdb1d9c77393d1ee83041c15
Walter-Britos1 <walterbritos418@gmail.com>
2023-08-26 21:20:48 -0300

    se agregaron correcciones

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const { insertCountryToDB } = require('./src/utils/api')
-const PORT = 3001;
+require('dotenv').config()
 
 // Sincronizar la base de datos y luego iniciar el servidor
 conn.sync({ alter: true }).then(() => {
   // Iniciar el servidor
-server.listen(PORT, async () => {
+server.listen(process.env.PORT, async () => {
   // Insertar países en la base de datos antes de escuchar en el puerto
   await insertCountryToDB()
   console.log('Insert completed');
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port`, process.env.PORT);
 })
 }).catch(error => console.error(error))
